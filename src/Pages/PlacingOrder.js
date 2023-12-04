e9bf88579b7149606106c8167eb2f4cd56fdc45d
kaleem <kaleem1999@outlook.com>
2023-12-04 22:13:53 +0200

    new changes

--- src/Pages/PlacingOrder.js
+++ src/Pages/PlacingOrder.js
@@ -15,8 +15,8 @@ import { db } from "../database/config";
 import jwtDecode from "jwt-decode";
 import { clearAllData, getData } from "../Helpers/localStorage";
 import { checkForOrders } from "../Helpers/Common";
-import addNotification from "react-push-notification";
-import { Notifications } from "react-push-notification";
+// import addNotification from "react-push-notification";
+// import { Notifications } from "react-push-notification";
 function triggerPushNotification() {
   const data = {
     type: "pushNotification",
@@ -50,19 +50,19 @@ const PlacingOrder = ({
       deleteDoc(docRef);
     }, 8000);
   };
-  const buttonClick = () => {
-    setTimeout(() => {
-      console.log(1000);
-      addNotification({
-        title: "Warning",
-        subtitle: "This is a subtitle",
-        message: "This is a very long message",
-        theme: "darkblue",
-        // native: true, // when using native, your OS will handle theming.
-      });
-      // triggerPushNotification();
-    }, 6000);
-  };
+  // const buttonClick = () => {
+  //   setTimeout(() => {
+  //     console.log(1000);
+  //     addNotification({
+  //       title: "Warning",
+  //       subtitle: "This is a subtitle",
+  //       message: "This is a very long message",
+  //       theme: "darkblue",
+  //       // native: true, // when using native, your OS will handle theming.
+  //     });
+  //     // triggerPushNotification();
+  //   }, 6000);
+  // };
   const checkStatus = () => {
     return (
       <>
@@ -224,10 +224,10 @@ const PlacingOrder = ({
               }}
             >
               <p style={styles.text3}>No Current Orders.</p>
-              <button onClick={buttonClick} className="button">
+              {/* <button onClick={buttonClick} className="button">
                 Hello world.
               </button>
-              <Notifications />
+              <Notifications /> */}
             </div>
           </>
         )}
